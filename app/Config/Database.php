0f07642bfa7497bd7f2690940c2a7bb5c56b0851
Omerta2005 <mandaomerta@gmail.com>
2026-07-20 09:26:58 +0300

    modeles

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -41,7 +41,7 @@ class Database extends Config
         'compress'     => false,
         'strictOn'     => false,
         'failover'     => [],
-        'port'         => 0, // pas de port en SQLite
+        'port'         => 0,
         'numberNative' => false,
         'foundRows'    => false,
         'dateFormat'   => [
